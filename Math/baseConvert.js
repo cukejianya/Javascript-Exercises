2fe4f100259e5b106f21232f6f7e02fa040aa503
Chinedum Mac <cukejianya@gmail.com>
2015-01-24 21:41:43 -0500

    Work on Math/baseConvert.js

--- Math/baseConvert.js
+++ Math/baseConvert.js
@@ -1,3 +1,20 @@
 function baseConvert(num, base1, base2){
-	if ()
+	if ((2 <= base1 && base1<= 36)||(2 <= base2 && base2<= 36)) {
+		num = num.toString().split("");
+		var baseX = 0;
+
+		for(var i = num.length-1; i >= 0; i--) {
+			console.log(parseInt(num[i]));
+			if (parseInt(num[i]) == isNaN) {
+				var charToInt = num[i].toUpperCase().charCode(0)-55;
+				num.splice(i, 1, charToInt);
+				console.log(num + "Yo");
+			}
+			// baseX += Math.pow(baseX, i)*num[i];
+		}
+
+	} else {
+		console.log("Please pick bases between 2 and 36");
+	}
 }
+baseConvert("432F", 3, 4);
