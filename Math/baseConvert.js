cc5077252b3c9430572795ffbf520addc27ef0b6
Chinedum Mac <cukejianya@gmail.com>
2015-01-24 23:48:51 -0500

    Finish Math/baseconvert.js

--- Math/baseConvert.js
+++ Math/baseConvert.js
@@ -1,20 +1,46 @@
 function baseConvert(num, base1, base2){
 	if ((2 <= base1 && base1<= 36)||(2 <= base2 && base2<= 36)) {
+		
 		num = num.toString().split("");
 		var baseX = 0;
+		var charToInt;
+		var power = num.length-1; // var for exponent when converting base1 to base of 10;
 
-		for(var i = num.length-1; i >= 0; i--) {
-			console.log(parseInt(num[i]));
-			if (parseInt(num[i]) == isNaN) {
-				var charToInt = num[i].toUpperCase().charCode(0)-55;
+		//this for loop does a few things. It first checks to see if the digit is a char or a int. If it is a char, using ACSII, we substract 55 from the char code which gives us our deserved digit in base 10. If it is a int, then parseInt to a datatype int.
+		//Afterwords, it replaces the array element with the new int.
+		//Then we add up the digits multiplied by the corresponding base powered.
+		for (var i = 0; i < num.length; i++) {
+			var tooBig = num[i]; // var just incase digit is too big since we 						change num[i] to int
+			if (isNaN(parseInt(num[i]))) {
+				charToInt = num[i].toUpperCase().charCodeAt(0)-55;
 				num.splice(i, 1, charToInt);
-				console.log(num + "Yo");
+			} else {
+				charToInt = parseInt(num[i]);
 			}
-			// baseX += Math.pow(baseX, i)*num[i];
+			//check to see if digit actually belongs to base1 digit
+			if (base1 > num[i]) { 
+				num.splice(i, 1, charToInt);
+				baseX += Math.pow(base1, power-i)*num[i];
+			} else {
+				return tooBig+" is too big to be a digit of base "+base1;
+			}
 		}
 
-	} else {
-		console.log("Please pick bases between 2 and 36");
-	}
+		num = baseX;
+		y = [];
+		var base2Conversion = function(num, base2) {
+			x = parseInt(num/base2);
+			if (x !== 0){
+				base2Conversion(x, base2);
+			}
+			y.push(num%base2);
+		};
+		base2Conversion(num, base2);
+		y = y.join("");
+		return y;
+
+    } else {
+    	console.log("Please pick bases between 2 and 36");     
+    } 
 }
-baseConvert("432F", 3, 4);
+console.log(baseConvert("F63", 16, 2));
