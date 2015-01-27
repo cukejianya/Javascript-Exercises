a34e63bc1fc8f70640ba5b02a7254d3118f63777
Chinedum Mac <cukejianya@gmail.com>
2015-01-27 08:34:53 -0500

    Finish Array/lastPartArray.js

--- Array/lastPartArray.js
+++ Array/lastPartArray.js
@@ -1,14 +1,18 @@
-function firstPartArray(arr,n) {
-	if ( (Array.isArray(arr) === false) || 
-		(typeof n !== "number") &&
-		(typeof n !== "undefined") )
-		return "please put in the right args";
-	if (n < 0 || typeof n === "undefined")
-		n =1;
-	if (n > arr.length)
-		n = arr.length;
+function lastPartArray(arr,n) {
+	if (arr == null || Array.isArray(arr) === false)
+		return void 0;
+	if (n == null || typeof n !== "number")
+		return [];
+	if (n < 0)
+		n = 1;
 	return arr.slice(0, n);
 }
 var x = [2,3,4,5,"arr",3,4,5345,2];
 
-console.log(firstPartArray(x,6));
+console.log(lastPartArray(x,6));
+console.log(lastPartArray());
+console.log(lastPartArray("yo",6));
+console.log(lastPartArray(x,323942034832));
+console.log(lastPartArray(x));
+console.log(lastPartArray(x,"adfakjhdf"));
+console.log(lastPartArray(x,-2));
