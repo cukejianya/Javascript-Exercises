60c1e063371fd266ed7b2f7d5064d1ecca974cc0
Chinedum Mac <cukejianya@gmail.com>
2015-02-03 23:58:47 -0500

    Add somethings to Array/differenceTwoArrays.js

--- Array/differenceTwoArrays.js
+++ Array/differenceTwoArrays.js
@@ -1,3 +1,8 @@
-function differenceTwoArrays(){
-	
+function differenceTwoArrays(arr1, arr2){
+	if (Array.isArray(arr1) === "true" && Array.isArray(arr2)==="true") {
+		var maxLength = Math.max(arr1.length, arr2.length); 
+		if () {
+			
+		}
+	}
 }
