54a76532729d83311c080896f2f8937edc3ad322
ChinedumTrove <chinedum@trove.com>
2015-08-04 13:23:35 -0400

    Finish first question | Start second question

--- EloquentJS/chapter6.js
+++ EloquentJS/chapter6.js
@@ -193,7 +193,7 @@ Object.defineProperty( TextCell.prototype,  'heightProp', {
 var cell = new TextCell("no\nway\nJose");
 console.log(cell.heightProp);
 
-console.log('\nInheritance');//-------------------
+console.log('\nSection | Inheritance');//-------------------
 function RTextCell(text) {
   TextCell.call(this, text);
 }
@@ -230,3 +230,60 @@ console.log('\nSection | Instance Of');//-------------------
 console.log("RTextCell(A) is a instance of RTextCell\n"+ (new RTextCell("A") instanceof RTextCell) );
 console.log("RTextCell(A) is a instance of TextCell\n"+(new RTextCell("A") instanceof TextCell) );
 console.log("TextCell(A) is a instance of RTextCell\n"+(new TextCell("A") instanceof RTextCell) );
+
+// ------Questions-----
+var question = '\nQuestion 1 - A Vector Type';
+//Write a constructor Vector that represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it should save to properties of the same name.
+
+//Give the Vector prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (the one in this and the parameter) x and y values.
+
+//Add a getter property length to the prototype that computes the length of the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+console.log(question);
+
+// Your code here.
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function (otherVector) {
+  return new Vector(this.x + otherVector.x, this.y + otherVector.y);
+};
+Vector.prototype.minus = function (otherVector) {
+  return new Vector(this.x - otherVector.x, this.y - otherVector.y);
+};
+
+Object.defineProperty( Vector.prototype, 'length', {
+  get: function() { return Math.sqrt(this.x*this.x + this.y*this.y) }
+})
+
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
+
+var question = '\nQuestion 2 - Another Cell';
+//Implement a cell type named StretchCell(inner, width, height) that conforms to the table cell interface described earlier in the chapter. It should wrap another cell (like UnderlinedCell does) and ensure that the resulting cell has at least the given width and height, even if the inner cell would naturally be smaller.
+console.log(question);
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.wid = width;
+  this.hgt = height;
+}
+
+StretchCell.prototype.minWidth = function () {
+  return Math.max(this.inner.minWidth(), this.wid);
+}
+StretchCell.prototype.minHeight = function () {
+  return Math.max(this.inner.minHeight(), this.hgt)
+}
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log(sc.minWidth());
+// → 3
+console.log(sc.minHeight());
+// → 2
+console.log(sc.draw(3, 2));
+// → ["abc", "   "]
