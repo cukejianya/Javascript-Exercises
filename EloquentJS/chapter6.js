53eea77f6b2b5026c34cc3a781800df7b7d5a66b
ChinedumTrove <chinedum@trove.com>
2015-08-04 17:58:05 -0400

    Finish 3rd question in chapter6

--- EloquentJS/chapter6.js
+++ EloquentJS/chapter6.js
@@ -290,3 +290,57 @@ console.log(sc.minHeight());
 // → 2
 console.log(sc.draw(3, 2));
 // → ["abc", "   "]
+
+var question = '\nQuestion 3 - Sequence Interface';
+//Design an interface that abstracts iteration over a collection of values. An object that provides this interface represents a sequence, and the interface must somehow make it possible for code that uses such an object to iterate over the sequence, looking at the element values it is made up of and having some way to find out when the end of the sequence is reached.
+
+//When you have specified your interface, try to write a function logFive that takes a sequence object and calls console.log on its first five elements—or fewer, if the sequence has fewer than five elements.
+
+//eThen implement an object type ArraySeq that wraps an array and allows iteration over the array using the interface you designed. Implement another object type RangeSeq that iterates over a range of integers (taking from and to arguments to its constructor) instead.
+console.log(question);
+
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++) {
+    if (!sequence.current()) {
+      break;
+    }
+    console.log(sequence.current());
+    sequence.next();
+  }
+}
+
+function ArraySeq(array) {
+  this.array = array;
+  this.pos = 0;
+}
+ArraySeq.prototype.next = function() {
+  this.pos++;
+}
+ArraySeq.prototype.current = function() {
+  return this.array[this.pos];
+}
+
+function RangeSeq(start, end) {
+  this.curr = start;
+  this.end = end;
+}
+RangeSeq.prototype.next = function() {
+  if (this.end > this.curr) {
+    this.curr++;
+  } else {
+    this.curr = false;
+  }
+}
+RangeSeq.prototype.current = function() {
+  return this.curr;
+}
+
+logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
