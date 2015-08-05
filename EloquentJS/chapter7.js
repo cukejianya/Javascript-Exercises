aa6994cda3a5456750beb24f3975203f5675ee35
Chinedum Mac <cukejianya@gmail.com>
2015-08-05 10:11:19 -0400

    Start chapter 7

---
+++ EloquentJS/chapter7.js
@@ -0,0 +1,33 @@
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+    vector.y >= 0 && vector.y < this.width;
+};
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
