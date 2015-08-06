d86abfe6c076cdb1148acc842ad9831e10e70d0c
Chinedum Mac <cukejianya@gmail.com>
2015-08-06 10:00:40 -0400

    Add Math/happyNumbers.js

---
+++ Math/happyNumbers.js
@@ -0,0 +1,17 @@
+
+function happyNumbers(num) {
+  num +='';
+  if (num === '1') {
+    return 'its a happy number';
+  } else if (num.length < 1) {
+    return 'Number is too sad.';
+  }
+  num = num.split('')
+    .map(function(a) { return parseInt(a); })
+    .reduce( function(a, b) {return a+b;} );
+  return happyNumbers(num);
+}
+
+var a = parseInt(process.argv.slice(2));
+
+console.log(happyNumbers(a));
