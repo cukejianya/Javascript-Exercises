7b2a3e298c64a0fbc050fd0da3347896e26a0786
Chinedum <cukejianya@gmail.com>
2015-10-30 02:40:04 -0400

    Finish httpUpperCaserer,js

--- NodeSchool/NodeBasic/httpUppercaserer.js
+++ NodeSchool/NodeBasic/httpUppercaserer.js
@@ -1,7 +1,18 @@
 var fs = require('fs');
 var http = require('http');
+// var util = require('util');
 
 var server = http.createServer(function(req, res) {
-  if (res.method === 'POST')
-  var file = fs.createReadStream(process.argv[2])
+  if (req.method === 'POST') {
+    // console.log(util.inspect(req) +'\n\n'+ util.inspect(res))
+    req.setEncoding('utf8');
+    req.on('data', function pipe2Array(data) {
+      data = data.toUpperCase();
+      res.write(data);
+    });
+    req.on('end', function done(){
+      res.end();
+    });
+  }
 })
+server.listen(process.argv[2])
