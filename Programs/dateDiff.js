b16f1475ca5a169bc61ecc1f60d9302a3dea493e
Chinedum <cukejianya@gmail.com>
2015-10-31 14:30:39 -0400

    Add dateDiff.js for schoolWeek.j

---
+++ Programs/dateDiff.js
@@ -0,0 +1,14 @@
+function dateDiff(startDate, endDate) {
+  startDate.setMilliseconds(0), startDate.setSeconds(0), startDate.setMinutes(0), startDate.setHours(0);
+  endDate.setMilliseconds(0), endDate.setSeconds(0), endDate.setMinutes(0), endDate.setHours(0);
+
+  var yearDiff, monthsDiff, daysDiff;
+
+  yearDiff = endDiff.getFullYear() - startDate.getFullYear();
+
+  var diffDict = {
+    startDiff = endDate.getTime()-startDate.getTime();
+  }
+
+
+}
