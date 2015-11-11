5cc9f9b9a10ff49d2156267916dac75ff30f09fe
Chinedum <cukejianya@gmail.com>
2015-11-11 14:44:18 -0500

    Finally finish the recursion problem

--- NodeSchool/Functional/improveRecursion.js
+++ NodeSchool/Functional/improveRecursion.js
@@ -1,11 +1,36 @@
-function getDependencies(fn, tree) {
-
+function getDependencies(tree) {
+  return Object.keys(reduceFn(tree, {})).sort();
 }
 
-function trampoline(fn){
-  while(fn && fn instanceOf Function){
-    fn =  fn();
-  }
-  return fn();
+function reduceFn(parentNode, dict) {
+  if (parentNode && parentNode.dependencies instanceof Object)
+    var depArr = Object.keys(parentNode.dependencies)
+  else return dict
+  return depArr.reduce(function theFn(dict, a, idx) {
+    var newNode = parentNode.dependencies[a]
+    if (!dict[a+'@'+newNode.version])
+      dict[a+'@'+newNode.version] = true;
+    if (newNode.dependencies) {
+      reduceFn(newNode, dict);
+    }
+    return dict;
+  }, dict);
 }
+
 module.exports = getDependencies
+
+//The official Solution. This code is so much cleaner!
+/*
+function getDependencies(mod, result) {
+  result = result || []
+  var dependencies = mod && mod.dependencies || []
+  Object.keys(dependencies).forEach(function(dep) {
+    var key = dep + '@' + mod.dependencies[dep].version
+    if (result.indexOf(key) === -1) result.push(key)
+    getDependencies(mod.dependencies[dep], result)
+  })
+  return result.sort()
+}
+
+module.exports = getDependencies
+*/
