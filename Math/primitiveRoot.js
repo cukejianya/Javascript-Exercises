d8605ab9ee58e46d51a0499a3db7f32ec4dfe5af
Chinedum <cukejianya@gmail.com>
2015-11-17 16:43:39 -0500

    Add a function/module that finds primitiveRoots of numbers

---
+++ Math/primitiveRoot.js
@@ -0,0 +1,27 @@
+var phifunc = require('./phiFunction');
+
+var p = console.log
+
+function primitiveRoot(num) {
+  var rss = phifunc(num);
+  var phi = rss.length;
+  rss.shift();
+
+  var roots = rss.filter(function(a){
+    for(var i = 1; i <= phi; i++) {
+      var mod = Math.pow(a,i) % num;
+      if (mod === 1) break;
+    }
+    if(i === phi) return true; else return false;
+  })
+
+  return roots;
+}
+
+module.exports = primitiveRoot;
+
+if (!module.parent) {
+  var num = parseInt(process.argv[2]);
+  var roots = primitiveRoot(num).join(', ');
+  console.log('Primitive Root(s): '+roots);
+}
