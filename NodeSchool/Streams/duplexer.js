43710b51305bf903ac78cd9be3107f47b30cc5d0
Chinedum <cukejianya@gmail.com>
2015-11-29 23:57:53 -0500

    Still trying to figure this duplex thing out

--- NodeSchool/Streams/duplexer.js
+++ NodeSchool/Streams/duplexer.js
@@ -1,52 +1,30 @@
 var spawn = require('child_process').spawn;
 var duplex = require('stream').Duplex;
 var util = require('util');
+var dx = require('duplexer2')
 
 util.inherits(Duplexer, duplex);
 
 function Duplexer(writable, readable) {
   duplex.call(this);
-
-  var self = this;
-
   this._writable = writable;
   this._readable = readable;
-  this._waiting = false;
 
-  this.on('end', function(){
-    this.stdin.pause();
-  }):
-
-  if (typeof readable.read !== "function") {
-    readable = (new stream.Readable(options)).wrap(readable);
-  }
-
-  readable.on('readable', function() {
-    if(self._waiting) {
-      writable.end();
-      self._read();
-    }
+  this.on('end', function () {
+    this._writable.pause();
   });
-  readable.once('end', function() {
-    self.push(null);
-  });
-
-  writable.once('finish', function() {
-    self.end();
-  });
-
-
 }
 
 Duplexer.prototype._read = function _read(n)  {
-  this._readable.read(n);
+  return this._readable.read(n);
 }
 Duplexer.prototype._write = function _write(input, encoding, done) {
   this._writable.write(input, encoding, done);
 }
+
 module.exports = function (cmd, args) {
   var child = spawn(cmd, args);
   var duplexer = new Duplexer(child.stdin, child.stdout);
 
-  return duplexer;
+  return
 };
