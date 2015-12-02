f7d42fb484bb91d124a8114b629b898b4910e5a7
Chinedum <cukejianya@gmail.com>
2015-12-01 23:15:54 -0500

    Finsih 11 exercises of stream

--- NodeSchool/Streams/duplexer.js
+++ NodeSchool/Streams/duplexer.js
@@ -1,7 +1,7 @@
 var spawn = require('child_process').spawn;
 var duplex = require('stream').Duplex;
 var util = require('util');
-var dx = require('duplexer2')
+var dx = require('duplexer')
 
 util.inherits(Duplexer, duplex);
 
@@ -10,21 +10,18 @@ function Duplexer(writable, readable) {
   this._writable = writable;
   this._readable = readable;
 
-  this.on('end', function () {
-    this._writable.pause();
-  });
 }
 
 Duplexer.prototype._read = function _read(n)  {
   return this._readable.read(n);
 }
 Duplexer.prototype._write = function _write(input, encoding, done) {
-  this._writable.write(input, encoding, done);
+  return this._writable.write(input, encoding, done);
 }
 
 module.exports = function (cmd, args) {
   var child = spawn(cmd, args);
   var duplexer = new Duplexer(child.stdin, child.stdout);
 
-  return
+  return dx(child.stdin, child.stdout);
 };
