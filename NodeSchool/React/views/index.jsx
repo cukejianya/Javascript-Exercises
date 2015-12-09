2b0dff2f068aa5c36223e2f683b02873065546bd
Chinedum <cukejianya@gmail.com>
2015-12-09 14:44:05 -0500

    Finished exercise 5 of react

--- NodeSchool/React/views/index.jsx
+++ NodeSchool/React/views/index.jsx
@@ -35,9 +35,21 @@ class TodoForm extends React.Component {
 }
 
 class Todo extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {checked: false};
+  }
+
+  handleChange(e) {
+    this.setState({checked: e.target.checked});
+  }
+
   render() {
     return (
       <tr>
+        <td style={{border: "1px solid black"}}>
+          <input type="checkbox" checked={this.state.checked} onCahnge={this.handleChange}/>
+        </td>
         <td style ={{border: "1px solid black"}}>{this.props.title}</td>
         <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
