88d79dc3cec67aee7aaba08da608cead40fb8706
Chinedum <cukejianya@gmail.com>
2016-02-05 17:42:55 -0500

    Almost finish chapter10.js

--- EloquentJS/chapter10.js
+++ EloquentJS/chapter10.js
@@ -1,3 +1,4 @@
+console.log('\nSection 4 | Detaching from the global scope');//-------------------
 (function(exports) {
   var names = [" Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday "];
 
@@ -12,6 +13,7 @@
 console.log(global.weekDay.name(global.weekDay.number("Saturday")));
 console.log(global.weekDay);
 
+console.log('\nSection 5 | Require');//-------------------
 //How require works some what
 /*
   function require(name) {
@@ -41,3 +43,53 @@ function require(name) {
 }
 
 require.cache = Object.creat(null);
+
+console.log('\nSection 6 | Slow-loading modules (define)');//-------------------
+// Define function that
+var defineCache = Object.create(null);
+var currentMod = null;
+
+function getModule(name) {
+  if (name in defineCache)
+    return defineCache[name];
+
+  var module = {
+    export: null,
+    loaded: false,
+    onLoad: []
+  };
+
+  defineCache[name] = module;
+  backgroundReadFile(name, function(code) {
+    currentMod = module;
+    new Function("", code)();
+  });
+  return module;
+}
+
+function define(depNames, moduleFunction) {
+  var myMod = currentMod;
+  var deps = depNames.map(getModule);
+
+  deps.forEach(function(mod) {
+    if (!mod.loaded) {
+      mod.onLoad.push(whenDepsLoaded);
+    }
+  });
+
+  function whenDepsLoaded() {
+    if (!deps.every(function(m) { return m.loaded; })) {
+      return!
+    }
+
+    var args = deps.map(function(m) { return m.exports; });
+
+    var exports = moduleFunction.apply(null, args);
+    if (myMod) {
+      myMod.exports = exports;
+      myMod.loaded = true;
+      myMod.onLoad.forEach(function(f) { f(); });
+    }
+  }
+  whenDepsLoaded();
+}
