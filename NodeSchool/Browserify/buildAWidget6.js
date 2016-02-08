307416a6dcb67497c6f9ebecb43b44a0942a899b
Chinedum <cukejianya@gmail.com>
2016-02-07 22:57:35 -0500

    Finish 6/10 browserify adv

--- NodeSchool/Browserify/buildAWidget6.js
+++ NodeSchool/Browserify/buildAWidget6.js
@@ -1,17 +1,18 @@
 var domify = require('domify');
 
 function Widget() {
-  that = this;
-  this.html = domify('<div>Hello <span class="name"></span>!</div>');
-
-  this.setName = function(str) {
-    that.html.querySelector(".name").textContent = str;
+  if (!(this instanceof Widget)) {
+    return new Widget();
   }
-
-  this.appendTo = function(target){
-    target.appendChild(that.html);
-  };
-
+  this.html = domify('<div>Hello <span class="name"></span>!</div>');
 }
 
-module.exports = Widget
+Widget.prototype.setName = function(str) {
+  this.html.querySelector(".name").textContent = str;
+}
+
+Widget.prototype.appendTo = function(target){
+  target.appendChild(this.html);
+}
+
+module.exports = Widget;
