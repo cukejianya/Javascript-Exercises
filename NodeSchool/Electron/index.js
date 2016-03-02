0e06beede67b56b5c5e22a67c88dbfa340d61a98
Chinedum <cukejianya@gmail.com>
2016-03-02 00:09:24 -0500

    Finish electron nodeschool

--- NodeSchool/Electron/index.js
+++ NodeSchool/Electron/index.js
@@ -1,8 +1,27 @@
-var picture = require('cat-picture');
+// var picture = require('cat-picture');
+//
+//
+// var src = picture.src;
+// picture.remove();
+//
+// var image = require('lightning-image-poly');
+//
+// var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
 
-var src = picture.src;
-picture.remove();
+var remote = require('electron').remote;
+var fs  = require('fs');
 
-var image = require('lightning-image-poly');
+function save() {
+  remote.getCurrentWindow().webContents.printToPDF({
+    portrait: true,
+  }, function (err, data) {
+    fs.writeFile('annotation.pdf', data, function (err) {
+      if (err) alert('error generating pdf! ' + err.message);
+      else alert('pdf saved!');
+    });
+  });
+}
 
-var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode === 80) save();
+});
