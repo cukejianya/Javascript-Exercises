81b078918d1c59ffa920d8b939a3ef3c9d2a79c5
Chinedum <cukejianya@gmail.com>
2016-03-18 23:52:14 -0400

    Add linearTrig.js

---
+++ Math/linearTrig.js
@@ -0,0 +1,21 @@
+function linCos(base, rotation) {
+  var period = base * 2;
+  var phase = rotation % period;
+  var distance = Math.abs(base - phase);
+  return distance*Math.sign(Math.cos(Math.PI / period * rotation));
+}
+
+function linSin(base, rotation) {
+  var period = base * 2;
+  var phase = rotation % period;
+  var distance = base - Math.abs(phase - base);
+  return distance*Math.sign(Math.sin(Math.PI / period * rotation));
+}
+
+function Coord(num) {
+  for (var i = 0; i < num*4; i++) {
+    console.log(linCos(num, i), linSin(num, i));
+  }
+}
+
+Coord(3);
