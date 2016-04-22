703aedcfc1e97eb867049aca930a1f65f5c9717a
Chinedum Ukejianya <cukejianya@gmail.com>
2016-04-22 14:01:51 -0400

    Finish part 3 of step 3 of think-in-react

--- NodeSchool/thinkInReact/UI.js
+++ NodeSchool/thinkInReact/UI.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const FilterableProductTable = React.createClass ({
   render () {
+    const products = this.props.products;
+
     return (
-      <SearchBar/>
+      <div>
+        <SearchBar/>
+        <ProductTable products={products}/>
+      </div>
     )
   }
 })
@@ -17,3 +22,67 @@ export const SearchBar = React.createClass ({
     )
   }
 })
+
+export const ProductTable = React.createClass ({
+  render () {
+    const products = this.props.products;
+    const rows = [];
+    let currentCategory;
+    products.forEach((product) => {
+      if (product.category !== currentCategory) {
+        currentCategory = product.category;
+
+        rows.push((
+          <ProductCategoryRow
+            key={currentCategory}
+            category={currentCategory}
+          />
+        ));
+      }
+      rows.push((
+        <ProductRow key={product.name} product={product}/>
+      ));
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
+    );
+  },
+});
+
+export const ProductRow = React.createClass({
+  render () {
+    const product = this.props.product;
+
+    const style = {
+      color: product.stocked ? null : 'red',
+    };
+
+    return (
+        <tr>
+            <td style={style}>{product.name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+  },
+});
+
+export const ProductCategoryRow = React.createClass({
+  render() {
+    return (
+        <tr>
+            <th colSpan={2}>{this.props.category}</th>
+        </tr>
+    );
+  },
+});
